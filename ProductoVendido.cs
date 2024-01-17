ecb98a76feb8928f370104e53ed5f88808091f69
franciscoCalarota <franciscocalarota@gmail.com>
2024-01-17 12:15:51 +0100

    Se agrega el mensaje en el main

--- ProductoVendido.cs
+++ ProductoVendido.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clase6
 {
-    public class ProductoVendido : Producto
+    public class ProductoVendido
     {
-        protected int id;
+        private int ID;
+        private int IDPRODUCTO;
+        private int STOCK;
+        private int IVENTA;
 
-        public ProductoVendido(int id, int IdProducto, int Stock, int IdVenta)  
-            : base(IdProducto,
-                   Stock,
-                   IdVenta)
+
+
+        private ProductoVendido(
+            int id,
+            int idproducto,
+            int stock,
+            int venta)
         {
-            this.id = id;
+            this.ID = id;
+            this.IDPRODUCTO = idproducto;
+            this.STOCK = stock;
+            this.IVENTA = venta;
         }
+        public int Id { get { return ID; } set { ID = value; } }
+        public int idProducto { get { return IDPRODUCTO; } set { IDPRODUCTO = value; } }
+
+        public int Stock { get { return STOCK; } set { STOCK = value; } }
+
+        public int idVenta { get { return IVENTA; } set { IVENTA = value; } }
     }
 }
